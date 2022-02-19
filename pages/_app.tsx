52ee5a0f63022da38521ae737d437d1cbd2c9ad8
Kristian Elset Bø <kristian.e.boe@gmail.com>
2022-02-19 11:37:47 +0100

    feat: added splitbee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import splitbee from '@splitbee/web';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    splitbee.init({
+      // To use Splitbee on another subdomain.
+      // Token can be found in project settings
+      token: process.env.NEXT_PUBLIC_SPLITBEE_TOKEN,
+
+      // Enable cookie-less mode. Defaults to `false`
+      disableCookie: false,
+
+      // Set custom urls when using proxying
+      scriptUrl: 'https://cdn.splitbee.io/sb.js',
+      apiUrl: 'https://hive.splitbee.io',
+    });
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ plan.md
@@ -0,0 +1,23 @@
+Other communities
+
+- https://www.remoteworkcommunity.com/
+- https://wifitribe.co/blog/remote-work-community/
+- https://www.remotelyone.com/
+- https://www.theremoteworkers.com/
+
+Great aggregate list
+https://www.indiehackers.com/post/16-best-remote-work-and-digital-nomad-communities-to-join-and-learn-from-d12ae8e694
+
+- https://www.unita.co/
+
+Other
+https://himalayas.app/
+https://growremote.ie/
+https://remotive.io/community
+https://www.outsite.co/blog/how-to-find-digital-nomad-communities
+https://dollarflightclub.com/digital_nomad_guides/15-best-digital-nomad-communities-to-join/
+https://digitalnomads.world/
+https://outandbeyond.com/digital-nomad-communities/
+
+Top 1
+https://nomadlist.com/
